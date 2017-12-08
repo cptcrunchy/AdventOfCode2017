59065144e6bab49ca4f017389048c083c23f7fb3
Jason Gutierrez <jasong84@gmail.com>
2017-12-08 01:53:07 -0600

    Day 7 solutions

--- Day7/Part1.js
+++ Day7/Part1.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
 fs.readFile(__dirname + '/input.txt', 'utf8', (err,data) => {
-  const apps = data.trim().split('\n').sort();
-  console.log(apps);
+  const disks = new Set();
+  const branches = new Set();
+
+  const apps = data.trim().split('\n').forEach( row => {
+    const sections = row.split(' -> ');
+    disks.add(sections[0].split(' ')[0].trim());
+
+    if(sections.length > 1) {
+      sections[1].split(',').map( x => branches.add(x.trim()));
+    }
+  });
+
+  branches.forEach( x => disks.delete(x));
+  console.log(disks.values().next().value);
 });
--- Day7/Part2.js
+++ Day7/Part2.js
@@ -1,5 +1,68 @@
 const fs = require('fs');
 
 fs.readFile(__dirname + '/input.txt', 'utf8', (err,data) => {
-  
+  const disks = {};
+
+  const app = data.trim().split('\n').forEach( row => {
+    const rows = row.split(' -> ');
+    const disk = rows[0].split(' ');
+    const name = disk[0].trim();
+    disks[name] = {
+      value: Number(disk[1].substr(1, disk[1].indexOf(')') - 1)),
+      children: [],
+      total: 0,
+    };
+
+    if(rows.length > 1) {
+      disks[name].children = rows[1].split(',').map( x => x.trim());
+    }
+  });
+
+  const root = getRoot(disks);
+  sumWeights(root, disks);
+  console.log(balance(root, disks));
 });
+
+const getRoot = disks => {
+  const keys = new Set(Object.keys(disks));
+  for(const key in disks) {
+    for(const i in disks[key].children) {
+      keys.delete(disks[key].children[i]);
+    }
+  }
+  return keys.values().next().value;
+}
+
+const sumWeights = (root, tree) => {
+  tree[root].total = tree[root].value;
+  tree[root].children.forEach( child => {
+    tree[root].total += sumWeights(child, tree);
+  });
+  return tree[root].total;
+}
+
+
+const balance = (root, tree, target) => {
+  const children = {};
+  let newRoot;
+  tree[root].children.forEach( child => {
+    if( children[tree[child].total] === undefined ) {
+      children[tree[child].total] = child;
+    }else {
+      children[tree[child].total] = false;
+      newRoot = tree[child].total;
+    }
+  });
+
+  for(const i in children) {
+    if( children[i]) {
+      return balance(children[i], tree, newRoot);
+    }
+  }
+
+  return tree[root].value + target - tree[root].total;
+}
+
+
+
+
